687656b00bb046fd3904f5ae9f18bfc57326725c
Mireia Majó i Cornet <78376277+MireiaMajoCornet@users.noreply.github.com>
2025-03-11 13:12:30 +0100

    Delete yolo/pred_yolov12.py

--- yolo/pred_yolov12.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("runs/detect/train/weights/best.pt")
-model.predict("/data/users/mireia/MCV/C5/KITTI-MOTS/testing/image_02/0000", save=True, imgsz=320, conf=0.5)
