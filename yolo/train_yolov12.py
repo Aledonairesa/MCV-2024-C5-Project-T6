ae05042c4652a8c26a2759e30d33bf4356f543d0
Mireia Majó i Cornet <78376277+MireiaMajoCornet@users.noreply.github.com>
2025-03-11 13:12:41 +0100

    Delete yolo/train_yolov12.py

--- yolo/train_yolov12.py
+++
@@ -1,7 +0,0 @@
-from ultralytics import YOLO
-
-# Load a COCO-pretrained YOLO12n model
-model = YOLO("yolo12m.pt")
-
-# Train the model on the COCO8 example dataset for 100 epochs
-results = model.train(data="kitti-mots.yaml", epochs=100, imgsz=320)
